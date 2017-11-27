65f7af5da66ec9848a2ff318939c896c86d2b9cd
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-27 16:19:16 -0500

    finished

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
 public class Problem4 {
+
+    public String winterIsComing(String string) {
+        int n = string.length();
+        int alphabet[] = new int[26]; //given the sample input, think i need to go through the alphabet? this probably only works for this specific problem :(.
+
+        //want to iterate through the length of the alphabet to check for odds and evens. if odd return NO, if even return YES.
+        for (int i = 0; i < string.length(); i++) {
+            alphabet[string.charAt(i) - 'a']++;
+        }
+        if (n % 2 == 0) {
+            for (int i = 0; i < 26; i++) {
+                if (alphabet[i] % 2 != 0) {
+                    return "NO";
+                }
+            }
+            return "YES";
+        }
+        else {
+            int x = 0;
+            for (int i = 0; i < 26; i++) {
+                if (alphabet[i] % 2 != 0 && x < 1) {
+                    x++;
+                } else if (alphabet[i] % 2 != 0) {
+                    return "NO";
+                }
+            }
+            return "YES";
+        }
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Problem4Test {
+
+    Problem4 problem4 = new Problem4();
+
+    @Test
+    public void winterIsComingYesTest() {
+        String expected = "YES";
+        String actual = problem4.winterIsComing("aaabbbb");
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void winterIsComingNoTest() {
+        String expected = "NO";
+        String actual = problem4.winterIsComing("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected,actual);
+    }
 }
