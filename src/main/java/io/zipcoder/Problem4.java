a0e2882d4dabe814fd06e8f7c8e94257f57d3d5a
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-29 08:59:01 -0500

    re-did problem with better solution

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,18 +1,44 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Problem4 {
 
-    public String winterIsComing(String string) {
-        int n = string.length();
-        int alphabet[] = new int[26]; //given the sample input, think i need to go through the alphabet? this probably only works for this specific problem :(.
+//    public String winterIsComing(String string) {
+//        int n = string.length();
+//        int alphabet[] = new int[26]; //given the sample input, think i need to go through the alphabet? this probably only works for this specific problem :(.
+//
+//        //want to iterate through the length of the alphabet to check for odds and evens. if odd return NO, if even return YES.
+//        for (int i = 0; i < string.length(); i++) {
+//            alphabet[string.charAt(i) - 'a']++;
+//        }
+//        if (n % 2 == 0) {
+//            for (int i = 0; i < 26; i++) {
+//                if (alphabet[i] % 2 != 0) {
+//                    return "NO";
+//                }
+//            }
+//            return "YES";
+//        }
+//        else {
+//            int x = 0;
+//            for (int i = 0; i < 26; i++) {
+//                if (alphabet[i] % 2 != 0 && x < 1) {
+//                    x++;
+//                } else if (alphabet[i] % 2 != 0) {
+//                    return "NO";
+//                }
+//            }
+//            return "YES";
+//        }
+//    }
 
-        //want to iterate through the length of the alphabet to check for odds and evens. if odd return NO, if even return YES.
-        for (int i = 0; i < string.length(); i++) {
-            alphabet[string.charAt(i) - 'a']++;
-        }
-        if (n % 2 == 0) {
-            for (int i = 0; i < 26; i++) {
-                if (alphabet[i] % 2 != 0) {
+    //better, different way to solve below
+    public String winterIsComing(String input) {
+        ArrayList<Character> distinctLetters = getDistinctLetters(input);
+        if (input.length() % 2 == 0) {
+            for (Character character : distinctLetters) {
+                if (character % 2 != 0) {
                     return "NO";
                 }
             }
@@ -20,14 +46,24 @@ public class Problem4 {
         }
         else {
             int x = 0;
-            for (int i = 0; i < 26; i++) {
-                if (alphabet[i] % 2 != 0 && x < 1) {
+            for (Character character : distinctLetters) {
+                if (character % 2 != 0 && x < 1) {
                     x++;
-                } else if (alphabet[i] % 2 != 0) {
+                } else if (character % 2 != 0) {
                     return "NO";
                 }
             }
             return "YES";
         }
+    }
+
+    private ArrayList<Character> getDistinctLetters(String input) {
+        ArrayList<Character> distinctLetters = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            if (!distinctLetters.contains(input.charAt(i))) {
+                distinctLetters.add(input.charAt(i));
+            }
+        }
+        return distinctLetters;
     }
 }
